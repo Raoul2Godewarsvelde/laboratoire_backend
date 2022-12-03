a6e21088139f4077eed4ceb898e976c328e7b136
Raoul <raouldegodewarsvelde@gmail.com>
2022-12-03 20:34:13 +0100

    INIT: Initializr

---
+++ src/main/java/com/moko/laboratoirebackend/LaboratoireBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moko.laboratoirebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratoireBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratoireBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moko/laboratoirebackend/LaboratoireBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moko.laboratoirebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratoireBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
