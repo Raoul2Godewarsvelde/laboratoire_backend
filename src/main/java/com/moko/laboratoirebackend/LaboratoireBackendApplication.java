849964e87ae59c1eadfb14fbdbf652de58b0eaf9
Raoul <raouldegodewarsvelde@gmail.com>
2022-12-03 21:35:53 +0100

    INIT: Hello world

--- src/main/java/com/moko/laboratoirebackend/LaboratoireBackendApplication.java
+++ src/main/java/com/moko/laboratoirebackend/LaboratoireBackendApplication.java
@@ -2,12 +2,19 @@ package com.moko.laboratoirebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class LaboratoireBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LaboratoireBackendApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String hello() {
+		return "Hello world!!!";
+	}
 }
